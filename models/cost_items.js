bc4a07e8ada07cf989dae370395617769201c1e8
Raziel <raziel.otik@gmail.com>
2025-02-04 18:29:33 +0200

    JSDoc cost items

--- models/cost_items.js
+++ models/cost_items.js
@@ -2,6 +2,19 @@ const mongoose = require('mongoose');
 const { getIntegerValidator } = require("../utils");
 const categories = ['food', 'health', 'housing', 'sport', 'education'];
 
+/**
+ * Cost schema definition.
+ * @typedef {Object} Cost
+ * @property {string} description - Description of the cost item.
+ * @property {string} category - Category of the cost item.
+ * @property {number} userid - User ID associated with the cost item.
+ * @property {number} sum - Sum of the cost item.
+ * @property {number} [year] - Year of the cost item.
+ * @property {number} [month] - Month of the cost item.
+ * @property {number} [day] - Day of the cost item.
+ * @property {string} [time] - Time of the cost item.
+ */
+
 const costSchema = new mongoose.Schema({
     description: {
         type: String,
