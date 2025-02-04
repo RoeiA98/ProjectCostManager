ba73d56895f5a6f32ae92db1167b85883b8d93be
Raziel <raziel.otik@gmail.com>
2025-02-04 19:10:54 +0200

    - Added some validation checks for user even tho none can add users except us. - JSDoc utils.js

--- utils.js
+++ utils.js
@@ -1,10 +1,17 @@
-
-// Define a named function to check if the value contains only letters in English
+/**
+ * Validates if a given value contains only English letters.
+ * @param {string} value - The value to be validated.
+ * @returns {boolean} True if the value contains only English letters, false otherwise.
+ */
 exports.isValidEnglishName = (value) => {
     return /^[a-zA-Z]+$/.test(value);
 };
 
-// Define a named function to check if the value contains only letters in English and spaces
+/**
+ * Returns a validator object to check if a value is an integer.
+ * @param {string} [message] - Custom error message.
+ * @returns {Object} Validator object with a validator function and a message.
+ */
 exports.getIntegerValidator = (message) => {
     return {
         validator: Number.isInteger,
