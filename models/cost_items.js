5fb040de29ad85ff66dc6134ff6502484b21e9d4
Roei <roei922@gmail.com>
2025-02-08 18:07:20 +0200

    fixed time error

--- models/cost_items.js
+++ models/cost_items.js
@@ -84,6 +84,7 @@ const costSchema = new mongoose.Schema({
         required: false,
         validate: {
             validator: function (value) {
+                if (!value) return true; // Allow empty values
                 const timeRegex = /^([01]\d|2[0-3]):([0-5]\d):([0-5]\d)$/;
                 return timeRegex.test(value);
             },
